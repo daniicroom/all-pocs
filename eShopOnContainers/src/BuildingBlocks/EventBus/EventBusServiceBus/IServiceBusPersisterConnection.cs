41d7a7e30d62ba8876c757f85403da4720da7ba8
daniicroom <81102444+daniicroom@users.noreply.github.com>
2024-03-28 18:34:01 -0500

    Delete eShopOnContainers directory

--- eShopOnContainers/src/BuildingBlocks/EventBus/EventBusServiceBus/IServiceBusPersisterConnection.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBusServiceBus;
-
-public interface IServiceBusPersisterConnection : IDisposable
-{
-    ServiceBusClient TopicClient { get; }
-    ServiceBusAdministrationClient AdministrationClient { get; }
-}
